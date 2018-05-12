2cf17126ad55979ab84895133dd6c295da7ada13
BuptStEve <2747745470@qq.com>
2018-05-13 00:46:00 +0800

    feat(examples, md): add webpack-two/

---
+++ examples/webpack-two/src/pages/index/index.js
@@ -0,0 +1,121 @@
+// index.js
+import { TuaPage } from 'tua-mp'
+import { log } from '@utils'
+
+import './index.less'
+
+let n = 0
+
+TuaPage({
+    data () {
+        return {
+            msg: 'msg',
+            a: {
+                b: 'a.b',
+            },
+            arr: [
+                { c: { d: 'd0' } },
+                { c: { d: 'd1' } },
+                1, 2, 3,
+            ],
+            g: 'hello world',
+
+            // 测试所需数据
+            testData: {
+                nested: {
+                    steve: 'steve',
+                    young: {
+                        young: 'young',
+                    },
+                },
+                arr: [],
+                nestedArr: [
+                    {
+                        name: 'steve',
+                        nick: {
+                            young: 'young',
+                        },
+                    },
+                    {
+                        name: 'jame',
+                    },
+                ],
+                insertNestedArr: [],
+            },
+        }
+    },
+    onLoad () {
+        log(this)
+        /* eslint-disable no-global-assign */
+        global = this
+        /* eslint-enable no-global-assign */
+
+        for (let i = 100; i > 90; i--) {
+            this.g = i
+            this['g'] = i + 1
+        }
+    },
+    computed: {
+        reversedG () {
+            return this.reverseStr(this.g)
+        },
+        gAndAB () {
+            return this.g + ' + ' + this.a.b
+        },
+        dataAndComputed () {
+            return this.g + ' + ' + this.reversedG
+        },
+    },
+    watch: {
+        msg (newVal, oldVal) {
+            log(`msg: ${oldVal} -> ${newVal}`)
+        },
+        'a.b' (newVal, oldVal) {
+            log(`a.b: ${oldVal} -> ${newVal}`)
+            setTimeout(() => {
+                this.msg = this.reverseStr(this.msg)
+            }, 1000)
+        },
+        'g' (newVal, oldVal) {
+            log(`g: ${oldVal} -> ${newVal}`)
+        },
+        'reversedG' (newVal, oldVal) {
+            log(`reversedG: ${oldVal} -> ${newVal}`)
+        },
+        'gAndAB' (newVal, oldVal) {
+            log(`gAndAB: ${oldVal} -> ${newVal}`)
+        },
+    },
+    methods: {
+        tapMsg () {
+            this.msg += n++
+        },
+        tapAB () {
+            this.a.b += n++
+        },
+        tapArr () {
+            this.arr.push(n++)
+        },
+        tapArrNest0 () {
+            this.arr[0].c.d = n++
+        },
+        tapArrNest1 () {
+            this.arr[1].c.d = n++
+        },
+        tapArrSp () {
+            this.arr.splice(2, 2, n++)
+        },
+        tapReverseG () {
+            this.g = this.reversedG
+        },
+        reverseStr (str) {
+            return String(str).split('').reverse().join('')
+        },
+        unshiftNested () {
+            this.arr.unshift({ c: { d: 'hey' } })
+        },
+        gotoLogs () {
+            wx.navigateTo({ url: '/pages/logs/logs' })
+        },
+    },
+})
---
+++ examples/webpack-two/webpack.config.babel.js
@@ -0,0 +1,153 @@
+import fs from 'fs'
+import path from 'path'
+import { BannerPlugin } from 'webpack'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import EslintFriendlyFormatter from 'eslint-friendly-formatter'
+import FriendlyErrorsWebpackPlugin from 'friendly-errors-webpack-plugin'
+
+import { pages } from './src/app/app.json'
+
+const resolve = (...dir) => path.resolve(__dirname, ...dir)
+
+// 从 src/ 直接拷贝到 dist/
+const fromPrefix = 'src/pages/*/*'
+const toPrefix = 'pages/[name]/[name]'
+const copyCfgArr = [
+    { from: 'src/assets', to: 'assets' },
+    { from: 'src/app/app.json', to: 'app.json' },
+    { from: `${fromPrefix}.wxml`, to: `${toPrefix}.wxml` },
+    { from: `${fromPrefix}.json`, to: `${toPrefix}.json` },
+    { from: `${fromPrefix}.wxss`, to: `${toPrefix}.wxss` },
+]
+
+// 页面入口
+const pagesEntry = pages
+    .map((page) => ({
+        [page]: resolve('src', page),
+    }))
+    .reduce((acc, cur) => ({ ...acc, ...cur }), {})
+
+export default ({ isDev }) => ({
+    mode: isDev ? 'development' : 'production',
+    stats: isDev ? 'none' : {
+        colors: true,
+        chunks: false,
+        modules: false,
+        children: false,
+        chunkModules: false
+    },
+    devtool: isDev && 'source-map',
+    entry: {
+        // app 应用入口
+        app: './src/app/app',
+        // pages 页面入口
+        ...pagesEntry,
+    },
+    output: {
+        filename: `[name].js`,
+        path: resolve('dist'),
+        globalObject: 'global',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'eslint-loader',
+                enforce: 'pre',
+                include: [resolve('src')],
+                options: {
+                    formatter: EslintFriendlyFormatter,
+                },
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [resolve('src')],
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader',
+                }),
+            },
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'less-loader',
+                    ],
+                }),
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                }),
+            },
+            {
+                test: /\.styl$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'stylus-loader',
+                    ],
+                }),
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            '@': resolve('src'),
+            '@const': resolve('src/scripts/const'),
+            '@utils': resolve('src/scripts/utils'),
+        },
+    },
+    watchOptions: {
+        aggregateTimeout: 300,
+    },
+    // 提取公共依赖
+    optimization: {
+        runtimeChunk: {
+            name: 'chunks/runtime'
+        },
+        splitChunks: {
+            cacheGroups: {
+                // npm 包
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'chunks/vendors',
+                    chunks: 'all',
+                },
+                // 项目公共函数
+                scripts: {
+                    test: /[\\/]src[\\/]scripts[\\/]/,
+                    name: 'chunks/scripts',
+                    chunks: 'all',
+                    // 强制提取
+                    enforce: true,
+                },
+            },
+        },
+    },
+    plugins: [
+        new CopyWebpackPlugin(copyCfgArr),
+        new ExtractTextPlugin('[name].wxss'),
+        new FriendlyErrorsWebpackPlugin(),
+        new BannerPlugin({
+            raw: true,
+            // 因为无法通过 html 的 script 标签插入
+            // 所以只好在入口文件 app.js 前插入公共依赖
+            banner: `require('./chunks/runtime');require('./chunks/vendors');require('./chunks/scripts');`,
+            include: 'app.js',
+        })
+    ],
+})
