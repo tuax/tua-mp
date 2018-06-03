694fd516ba1587bff568da57d42f443a10b49403
stevezyyang(杨振宇) <stevezyyang@tencent.com>
2018-06-03 11:24:33 +0800

    tmp

--- examples/webpack-simple/src/pages/index/index.js
+++ examples/webpack-simple/src/pages/index/index.js
@@ -8,7 +8,6 @@ const app = getApp()
 TuaPage({
     data () {
         return {
-            motto: 'Hello World',
             userInfo: {},
             hasUserInfo: false,
             canIUse: wx.canIUse('button.open-type.getUserInfo'),
@@ -24,6 +23,7 @@ TuaPage({
             // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
             // 所以此处加入 callback 以防止这种情况
             app.userInfoReadyCallback = ({ userInfo }) => {
+                console.log('userInfo', userInfo)
                 this.userInfo = userInfo
                 this.hasUserInfo = true
             }
