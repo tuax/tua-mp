39b201c7d0a1a0b4a7f91d9659969c31b9a5bc7c
BuptStEve <2747745470@qq.com>
2018-06-24 22:45:19 +0800

    feat(examples): ready to add tua-mp-service

---
+++ examples/webpack-simple/tua-mp.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    chainWebpack: (config) => {
+        // app 应用入口
+        config.entry('app')
+            // 因为默认入口是 ./src/app/index.js
+            .clear()
+            .add('./src/app/app').end()
+    },
+}
